d36a4d734c7e7076563731295b2f29e6c977f709
Raymond <workrayz@gmail.com>
2018-05-30 04:46:28 -0400

    Creating the Store

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "Components/App";
+import { injectGlobal } from "styled-components";
+import reset from "styled-reset";
+import "./typography";
+
+injectGlobal`
+${reset};
+body{
+    background-color:#ecf0f1;
+}`;
+
+ReactDOM.render(<App />, document.getElementById("root"));
